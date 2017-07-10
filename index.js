438b0af0a4622b8b2f108ef1113bae1948daaf04
JessDotJS <jessmanny19@gmail.com>
2017-07-10 14:18:35 -0400

    Initial Push

---
+++ index.js
@@ -0,0 +1,61 @@
+/**
+ * Created by jessdotjs on 10/07/17.
+ */
+
+
+// BASE SETUP
+// =============================================================================
+
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+var Wallet = require('./src/Wallet');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+
+// ROUTES FOR OUR API
+var router = express.Router();              // get an instance of the express Router
+
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/', function(req, res) {
+    res.json({ message: "Welcome to AmericanCoin's Api" });
+});
+
+
+/*
+ * Create
+ * Params - password: string
+ * */
+
+router.route('/create').post(function(req, res) {
+    var wallet = new Wallet();
+    var password = req.body.password;
+
+    if(password) {
+        res.json(wallet.create(password));
+    }else {
+        res.send(false);
+    }
+});
+
+
+
+
+
+
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
---
+++ src/Wallet.js
@@ -0,0 +1,44 @@
+/**
+ * Created by jessdotjs on 10/07/17.
+ */
+var ethereumjsWallet = require('ethereumjs-wallet');
+
+var Wallet = function (){
+
+};
+
+Wallet.prototype.create = function(password) {
+    const generatedWallet = ethereumjsWallet.generate([true]);
+    if(generatedWallet) {
+        var walletFile = generatedWallet.toV3String(password);
+
+        return {
+            privateKey: generatedWallet.getPrivateKey(),
+            publicKey: generatedWallet.getPublicKey(),
+            address: generatedWallet.getAddress(),
+            privateKeyString: generatedWallet.getPrivateKeyString(),
+            publicKeyString: generatedWallet.getPublicKeyString(),
+            addressString: generatedWallet.getAddressString(),
+            checksumAddress: generatedWallet.getChecksumAddressString(),
+            walletFileName: this.generateWalletName(),
+            walletFile: 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(walletFile))
+        }
+    }else {
+        return false;
+    }
+};
+
+Wallet.prototype.generateWalletName = function () {
+    const todayDate = new Date();
+    var dd = todayDate.getDate();
+    var mm = todayDate.getMonth() + 1; //January is 0!
+
+    const yyyy = todayDate.getFullYear();
+    if(dd < 10){
+        dd = '0' + dd;
+    }
+    if(mm < 10){
+        mm = '0' + mm;
+    }
+    return 'amc_wallet_' + mm +'-'+ dd + '-' + yyyy + '.json';
+};
